37648ab04d3e08744ea80eb0a609fb837b9ab6e8
Gulliz <62759878+Gulliz@users.noreply.github.com>
2026-09-14 14:13:53 +0200

    Update smoothie response display to use dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,8 @@ if totp:
 
      
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-    st.text(smoothiefroot_response.json())
+    #st.text(smoothiefroot_response.json())
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     
         
 
