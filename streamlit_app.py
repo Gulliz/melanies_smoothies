ebf1507adf7c9d9d90bf5b9f0c73f0bbb1044040
Gulliz <62759878+Gulliz@users.noreply.github.com>
2026-09-14 13:10:24 +0200

    Update streamlit_app.py
    json

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,6 +63,6 @@ if totp:
 
     import requests  
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
     
         
